d0f896042cdca3a9cd503898feeff4f90f58757f
Scott Johnson <baronvonvaderham@gmail.com>
2016-03-08 13:04:40 -0500

    Login/Logout Redirects
    Fixed login/logout link construction to properly redirect completed
    requests back to the page the user linked from to avoid the default
    django messages.
    
    Went ahead and abstracted the content block out of base.html into a new
    page layout template directory where I will store the layout templates, so
    the headers and nav bars and stuff can be shared between all via inheritence.
    Additional templates will similarly just have a content block in them
    with different regions and layouts, to be registered in models.py.

--- conference/content/models.py
+++ conference/content/models.py
@@ -6,21 +6,19 @@ from feincms.content.raw.models import RawContent
 from feincms.content.video.models import VideoContent
 from gallery.models import GalleryContent
 from types.logo_ticker import LogoTicker, SponsorLogo
+from types.parallax_box import ParallaxBox
 
 import os
 
 
 Page.register_templates({
     'title': _('Home Page'),
-    'path': 'base.html',
+    'path': 'page_layout_templates/home_page.html',
     'regions': (
         ('masterslider', _('Master Slider')),
-        ('call-to-action', _('Call to action bar')),
-        ('about-info', _('About info')),
-        ('parallax-quote', _('Parallax Quote')),
-        ('services', _('Services Area')),
-        ('team', _('Team')),
-        ('testimonials', _('Testimonials')),
+        ('call-to-action', _('Call To Action Bar')),
+        ('about', _('About')),
+        ('parallax', _('Parallax Box')),
         ('sponsors', _('Sponsors')),
         ('footer', _('Footer')),
     )
@@ -32,3 +30,4 @@ if os.environ.get('CMS', False):
     Page.create_content_type(GalleryContent)
     Page.create_content_type(VideoContent)
     Page.create_content_type(LogoTicker)
+    # Page.create_content_type(ParallaxBox)
