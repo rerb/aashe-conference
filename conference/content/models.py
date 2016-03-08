58eb9dbbd863960688fc2829f8dcbc1413896316
Scott Johnson <baronvonvaderham@gmail.com>
2016-03-08 13:12:37 -0500

    Cleanup
    Renamed a few things, commented out the in-progress content type
    "Parallax Box".

--- conference/content/models.py
+++ conference/content/models.py
@@ -6,7 +6,8 @@ from feincms.content.raw.models import RawContent
 from feincms.content.video.models import VideoContent
 from gallery.models import GalleryContent
 from types.logo_ticker import LogoTicker, SponsorLogo
-from types.parallax_box import ParallaxBox
+# from types.parallax_box import ParallaxBox
+from feincms.content.richtext.models import RichTextContent
 
 import os
 
@@ -15,7 +16,7 @@ Page.register_templates({
     'title': _('Home Page'),
     'path': 'page_layout_templates/home_page.html',
     'regions': (
-        ('masterslider', _('Master Slider')),
+        ('main_slider', _('Main Slider')),
         ('call-to-action', _('Call To Action Bar')),
         ('about', _('About')),
         ('parallax', _('Parallax Box')),
@@ -25,9 +26,10 @@ Page.register_templates({
 })
 
 if os.environ.get('CMS', False):
-    Page.create_content_type(MainSlider)
+    Page.create_content_type(MainSlider, regions='main_slider')
     Page.create_content_type(RawContent)
     Page.create_content_type(GalleryContent)
     Page.create_content_type(VideoContent)
-    Page.create_content_type(LogoTicker)
+    Page.create_content_type(LogoTicker, regions='sponsors')
     # Page.create_content_type(ParallaxBox)
+    Page.create_content_type(RichTextContent)
