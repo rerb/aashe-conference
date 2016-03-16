12af90d700b0f7914cd29bdabb4809dce5b16457
Scott Johnson <baronvonvaderham@gmail.com>
2016-03-15 22:36:34 -0400

    Clean up the clock
    Move javascript into custom.js to clean up base template.

--- conference/static/js/custom.js
+++ conference/static/js/custom.js
@@ -6,3 +6,15 @@ $(function(){
         $(this).removeClass('open');
     });
 });
+
+
+function getTimeRemaining(endtime) {
+  var t = Date.parse(endtime) - Date.parse(new Date());
+  return Math.floor(t / 1000)
+}
+var clock = $('.clock').FlipClock(
+        Math.floor((Date.parse(new Date("2016-10-9")) - Date.parse(new Date()))/1000), {
+    clockFace: 'DailyCounter',
+    countdown: true,
+    showSeconds: false
+});
