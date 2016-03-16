0f2a176358e60f46b36831744291e4e6d149d041
Scott Johnson <baronvonvaderham@gmail.com>
2016-03-16 12:50:07 -0400

    Issue #12: Call to Action Bar
    Fixed styling issues with content type, ready to go!

--- conference/content/types/call_to_action.py
+++ conference/content/types/call_to_action.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.template.loader import render_to_string
 from feincms.content.richtext.models import RichTextField
+from django.utils.safestring import mark_safe
 
 
 class CallToAction(models.Model):
@@ -13,7 +14,7 @@ class CallToAction(models.Model):
 
     def render(self, **kwargs):
         return render_to_string('call_to_action/call_to_action.html', {
-            'textbox': self.textbox,
+            'textbox': mark_safe(self.textbox),
             'link': self.link,
             'button_text': self.button_text,
         })
