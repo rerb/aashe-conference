2fe59470ab8e3911cac4ce45b289e14497272f9a
Scott Johnson <baronvonvaderham@gmail.com>
2016-04-06 16:51:20 -0400

    Issue #76
    Added an optional button for the featured pane content type.
    
    Also added styling to make it into a box to make it stand out more
    as a block.

--- conference/content/types/featured_pane.py
+++ conference/content/types/featured_pane.py
@@ -10,9 +10,11 @@ class FeaturedPane(models.Model):
         ('briefcase', 'Briefcase'),
     ]
 
-    icon = models.CharField(choices=ICON_CHOICES, max_length=10)
-    title = models.TextField(max_length=64)
-    text_block = models.TextField(max_length=256)
+    icon = models.CharField(choices=ICON_CHOICES, max_length=10, verbose_name="Select Icon")
+    title = models.TextField(max_length=64, verbose_name="Title")
+    text_block = models.TextField(max_length=256, verbose_name="Text Block")
+    button_url = models.URLField(blank=True, verbose_name="Button URL (optional)")
+    button_text = models.TextField(blank=True, max_length=32, verbose_name="Button Text")
 
     class Meta:
         abstract = True
@@ -23,4 +25,6 @@ class FeaturedPane(models.Model):
             'icon': 'icon-' + self.icon,
             'title': self.title,
             'text_block': self.text_block,
+            'button_url': self.button_url,
+            'button_text': self.button_text,
         })
