c0d66efd7b3039f7cc4bb1f6d86462d4c31f035b
Scott Johnson <baronvonvaderham@gmail.com>
2016-04-06 14:30:39 -0400

    Disable PricingTable again
    Need to flush all tables to apply migrations again.

--- conference/content/admin.py
+++ conference/content/admin.py
@@ -3,11 +3,11 @@ from django.contrib import admin
 from types.master_slider import SliderImage
 from types.logo_ticker import SponsorLogo, LogoCollection
 from types.header_box import HeaderBoxContent
-from types.pricing_table import RegistrationLevel, Deadline
+# from types.pricing_table import RegistrationLevel, Deadline
 
 admin.site.register(SponsorLogo)
 admin.site.register(SliderImage)
 admin.site.register(LogoCollection)
 admin.site.register(HeaderBoxContent)
-admin.site.register(RegistrationLevel)
-admin.site.register(Deadline)
+# admin.site.register(RegistrationLevel)
+# admin.site.register(Deadline)
