b99f7a8172c5f24542d0c3fe7041e038be753dcb
Scott Johnson <baronvonvaderham@gmail.com>
2016-04-20 15:51:14 -0400

    Issue #91

--- conference/content/models.py
+++ conference/content/models.py
@@ -183,7 +183,7 @@ if os.environ.get('CMS', False):
     Page.create_content_type(PricingTable, regions=('pricing_table',))
     Page.create_content_type(FeaturedPane, regions=('about', 'featured', 'content', 'additional_content',
                                                     'main_content', 'icon-featured-panes',
-                                                    'icon-featured-panes-row-2'))
+                                                    'icon-featured-panes-row-2', 'icon-featured-panes-row-1'))
     Page.create_content_type(TableContent,
                              regions=('about', 'content', 'main_content', 'additional_content',
                                       'rich-text-left-column', 'rich_text_content', 'rich_text_1', 'rich_text_2',))
