09896fc2831ae5872fbdeec940b8ccc53339da01
Benjamin Stookey <ben.stookey@gmail.com>
2016-04-21 22:29:19 -0600

    Added thumbnail support;

--- conference/settings.py
+++ conference/settings.py
@@ -143,7 +143,7 @@ USE_S3 = os.environ.get('USE_S3', None)
 if USE_S3:
     INSTALLED_APPS += ('s3_folder_storage',)
     from integration_settings.media.s3 import *
-    AWS_QUERYSTRING_AUTH = True
+    AWS_S3_SECURE_URLS = False
     FEINCMS_MEDIALIBRARY_UPLOAD_TO = os.path.join(DEFAULT_S3_PATH, 'medialibrary')
 
 else:
